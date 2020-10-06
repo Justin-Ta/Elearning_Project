0492f8bf73dbb387854dea06e5c039b4b7cb22a9
Thu <newzon263@gmail.com>
2020-10-06 22:10:00 +0700

    add header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/';
 import PageNotFound from './pages/PageNotFound';
 import { HomeTemplate } from './templates/homeTemplate';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,15 +1,55 @@
 import React from 'react';
-import { Row, Col } from 'antd';
-import { Layout } from 'antd';
-const { Content } = Layout;
+import { Input, Button, Typography } from 'antd';
+import { ShoppingCartOutlined } from '@ant-design/icons';
+
+import './styles.scss';
+
+const { Link, Text } = Typography;
+const { Search } = Input;
 
 
 export default function Header() {
     return (
-        <div>
-            <Row>
-                <div className="container">assdf</div>
-            </Row>
+        <div className="header">
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <Link href="#a" target="_blank" className="mr-auto">
+                    <img className="logo" src="./img/icon/icon_ELearning.ico" alt="" />
+                    <span>Learning</span>
+                </Link>
+
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon" />
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item ml-3 my-2">
+                            <Search
+                                placeholder="input search text"
+                                onSearch={value => console.log(value)}
+                                style={{ width: 300 }}
+                            />
+                        </li>
+                        <li className="nav-item ml-3 my-2">
+                            <a href="#">
+                                <Button className="loginBtn">
+                                    Log in
+                                </Button>
+                            </a>
+                        </li>
+                        <li className="nav-item ml-3 my-2">
+                            <a href="#">
+                                <Button type="primary" className="signupBtn">
+                                    Sign up
+                            </Button>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="ml-3 my-2 shopping-cart">
+                        <ShoppingCartOutlined />
+                </div>
+            </nav>
         </div>
     )
 }
