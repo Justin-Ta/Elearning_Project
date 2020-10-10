a6754893813baa90ac7e1ddb4d2297d3e641ca77
Justin-Ta <taquangduy1228@gmail.com>
2020-10-10 22:18:08 +0700

    BackToTop_Button

--- src/Components/BackToTop/index.jsx
+++ src/Components/BackToTop/index.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function BackToTop() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default class ScrollToTop extends Component {
+    
+    constructor(props){
+        super(props);
+        this.state={
+            is_visible: false,
+        }
+    }
+
+    componentDidMount(){
+        var scrollComponent =this;
+        document.addEventListener("scroll", function(e){
+        scrollComponent.toggleVisibility()
+        })
+    }
+    
+    toggleVisibility(){
+    if (window.pageYOffset>300){
+          this.setState({
+            is_visible: true
+        });
+        }else{
+          this.setState({
+            is_visible: false,
+        });
+    }
+    }
+
+    scrollToTop(){
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }
+    
+    render() {
+        const { is_visible } = this.state;
+        return (
+          <div className="scroll-to-top">
+            {is_visible && (
+              <div className="btn btn-primary"onClick={() => this.scrollToTop()}>
+                A
+              </div>
+            )}
+          </div>
+        );
+      }
 }
